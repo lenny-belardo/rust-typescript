9a527907efa1992901d9793f7e95f85207272c4c
Lenny Belardo <gheletus_raoul@yahoo.com>
2023-07-01 18:00:01 +0300

    [iterator-rs]: implemented a method that returns a list containing all its initial elements after adding 1 to them

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
+
+    let my_vec: Vec<isize> = vec![1, 2, 3]
+      .iter()
+      .map(|x| x + 1)
+      .collect();
+
+    println!("{:?}", my_vec);
 }

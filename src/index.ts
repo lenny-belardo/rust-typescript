55fef9b08665bcace57a459d76990941a4bac902
Lenny Belardo <gheletus_raoul@yahoo.com>
2023-07-07 18:15:29 +0300

    [interface-ts]: implemented a simple interface

--- src/index.ts
+++ src/index.ts
@@ -120,5 +120,33 @@ function printNumbersFromTxtFile() {
 
 printNumbersFromTxtFile();
 
+interface Area {
+    area(): number;
+}
+
+class Rectangle implements Area {
+    constructor(
+        public x: number,
+        public y: number,
+        public width: number,
+        public height: number
+    ){}
+
+    area(): number {
+        return this.width * this.height;
+    }
+}
+
+class Circle {
+    constructor(
+        public x: number,
+        public y: number,
+        public radius: number
+    ){}
+
+    area(): number {
+        return this.radius * this.radius * Math.PI;
+    }
+}
 
 

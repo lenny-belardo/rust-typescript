d477098dc8f04a8aeb35d7b8680d3f0a1c489ffb
Lenny Belardo <gheletus_raoul@yahoo.com>
2023-07-08 20:00:56 +0300

    [trait-rs]: implemented a simple default trait and its corresponding display

--- src/main.rs
+++ src/main.rs
@@ -211,18 +211,15 @@ fn main() {
 
     use crate::shapes::{area::Area, circle::Circle, rect::Rect};
 
-    let rect = Rect {
-        x: 0.0,
-        y: 0.0,
-        width: 10.0,
-        height: 10.0,
-    };
+    let rect = Rect::default();
 
     let circ = Circle {
         x: 0.0,
         y: 0.0,
         radius: 10.0,
     };
+
+    println!("{}", rect);
 
     println!("{}", circ.area());
     println!("{}", rect.area());
